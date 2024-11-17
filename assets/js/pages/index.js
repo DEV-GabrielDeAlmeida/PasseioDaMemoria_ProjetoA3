9013e2e92377c40f276263a7ce3254d2bc2286d6
Gabriel De Almeida Silva <gabrielsilvadev.dev@gmail.com>
2024-11-17 19:40:25 -0300

    aplicar script para login

---
+++ assets/js/pages/index.js
@@ -0,0 +1,26 @@
+const input = document.querySelector(".input-name");
+const btnPlay = document.querySelector(".btn-play");
+const btnMenu = document.querySelector(".btn-menu");
+
+const form = document.querySelector(".form-name");
+
+const validateInput = ({ target }) => {
+  if (target.value.length > 0) {
+    btnPlay.removeAttribute("disabled");
+    btnMenu.removeAttribute("disabled");
+    return;
+  }
+
+  btnPlay.setAttribute("disabled", "true");
+  btnMenu.setAttribute("disabled", "");
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  localStorage.setItem("playerName", input.value);
+  window.location = "html/fase_01_praia.html";
+};
+
+input.addEventListener("input", validateInput);
+form.addEventListener("submit", handleSubmit);
